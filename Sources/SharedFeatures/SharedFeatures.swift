3a290adb2025f4aaaf0d3e40fcf9e157feb9bf71
Douglas Adams <douglas.adams@me.com>
2022-11-29 10:01:03 -0500

    Initial Commit

---
+++ Sources/SharedFeatures/SharedFeatures.swift
@@ -0,0 +1,6 @@
+public struct SharedFeatures {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/SharedFeaturesTests/SharedFeaturesTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SharedFeatures
+
+final class SharedFeaturesTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SharedFeatures().text, "Hello, World!")
+    }
+}
